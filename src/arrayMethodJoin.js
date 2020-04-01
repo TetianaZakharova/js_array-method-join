35cb8dfd4ad341f0f638ebd90870698f56890b76
Tetiana Zakharova <kazakova.tanya@gmail.com>
2020-04-01 16:45:37 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,36 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let str = '';
+    let customSeparator = separator;
+
+    if (separator === undefined) {
+      customSeparator = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (!emptyNullElements(this[i])) {
+        str = str + this[i];
+      } else {
+        str = str + '';
+      }
+
+      if (this.length > (i + 1)) {
+        str += customSeparator;
+      }
+    }
+
+    return str;
   };
+
+  function emptyNullElements(value) {
+    return (
+      value === undefined
+      || value === null
+      || value === ''
+      || value.length === 0
+    );
+  }
 }
 
 module.exports = applyCustomJoin;
